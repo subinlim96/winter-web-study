f6b1e5a6db4c753139cc6715147b22fcd0199fcd
subinlim96 <wooys8624@naver.com>
2026-02-01 23:22:13 +0900

    update

--- practice/day2.js
+++ practice/day2.js
@@ -1,2 +1,9 @@
-var arr = []; // 빈 배열 생성
+const arr = []; // 빈 배열 생성
 
+arr[0] = '갱갱'; // <meta charset="UTF-8">는 한글 깨짐을 방지함.
+arr[1] = 'bin';
+arr[2] = 'tow';
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
