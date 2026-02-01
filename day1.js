086b52780c6a4c57c3b198e77e0e4a2c697ddd8c
subinlim96 <wooys8624@naver.com>
2026-02-01 12:00:54 +0900

    update

---
+++ day1.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < 11; i++) {
+    if (i % 2 !== 0)
+        continue;
+    console.log('${i}');
+}
+
+for (let i = 0; i < 11; i++) {
+    if (i % 2 == 0)
+        console.log('${i}');
+}
--- main.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello Winter Study!");
