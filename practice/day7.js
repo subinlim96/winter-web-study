70f351695f4fae6d4c74b543854444bf44d4211f
subinlim96 <wooys8624@naver.com>
2026-02-07 22:26:48 +0900

    update

--- practice/day7.js
+++ practice/day7.js
@@ -17,51 +17,57 @@ console.log('5. JSON 변환');
 var students = [];
 
 function showMenu() {
-    var students = [];
-
-    rl.question('선택 >', (number) => {
+    rl.question('선택 > ', (number) => {
         switch (number) {
             case "0":
+                rl.close();
                 break;
             case "1":
                 console.log('추가할 학생의 정보를 입력해주세용~~');
-                rl.question('이름 >', (name) => {
-                    rl.question('점수 >', (grade) => {
-                        students.splice(students.length, 1, ['이름: name', '점수: grade']);
+                rl.question('이름 > ', (name) => {
+                    rl.question('점수 > ', (grade) => {
+                        students.splice(students.length, 1, {이름: name, 점수: grade});
                         console.log(students);
-                    })
-                })
-                showMenu();
+                        showMenu();                  
+                    });
+                });
                 break;
             case "2":
                 console.log('제거할 학생의 정보를 입력해주세용~~');
-                rl.question('이름 >', (name) => {
+                rl.question('이름 > ', (name) => {
                     students.forEach((item, index) => {
-                        if (item.이름 == name)
+                        if (item.이름 == name) {
                             students.splice(index, 1);
                             console.log(students);
-                    })
-                })
-                showMenu();
+                            showMenu();
+                        }
+                    });
+                });
                 break;
             case "3":
                 var sum1 = 0;
 
                 for (var i of students)
-                    sum1 += i.점수;
+                    sum1 += parseInt(i.점수, 10);
                 console.log('현재 평균 점수는', sum1 / students.length, '점입니다.');
-                showMenu()
+                showMenu();
                 break;
             case "4":
                 var max1 = 0;
 
                 for (var i of students) {
-                    if (max1 < i.점수)
-                        console.log('현재 최고 점수는', sum1 / students.length, '점입니다.');
+                    if (max1 < parseInt(i.점수, 10))
+                        max1 = parseInt(i.점수, 10);
                 }
-                showMenu()
+                console.log('현재 최고 점수는', max1, '점입니다.');
+                showMenu();
                 break;
             case "5":
+                const jsonchan = JSON.stringify(students);
+                console.log(jsonchan);
+                showMenu();
+                break;
         }
     });
 }
+showMenu();
