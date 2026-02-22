af6b6f81964e6db4b35c9ebdf986c5c3b0cf5f49
subinlim96 <wooys8624@naver.com>
2026-02-22 16:11:16 +0900

    update

--- practice/day08.js
+++ practice/day08.js
@@ -1,33 +1,29 @@
-function keyCodeCheck() {
-    if (window.event.keyCode === 13) {
-        const newLi = document.createElement('li'); // li 생성
-        const newBtn = document.createElement('button'); // button 생성
-        const newSpan = document.createElement('span'); // span 생성
+const input = document.querySelector("#inputbar");
+const btn = document.querySelector("#button");
+const list = document.querySelector("#list");
 
-        newLi.appendChild(newBtn); // li안에 button 담기
-        newLi.appendChild(newSpan); // li안에 span 담기
-        console.log(newLi);
+btn.addEventListener("click", () => {
+    // 여기 안에 “input 값 가져오기 → li 만들기 → ul에 추가”를 넣을 것
+    const text = input.value.trim();
+    if (!text) return;
+    const li = document.createElement("li"); // li를 만드는 코드
+    li.textContent = text; // 안전하고 추천
+    list.appendChild(li);
+    // 또는 list.append(li);
+    input.value = "";
+    input.focus();
+});
 
-        const todoInput = document.querySelector('#todoInput');
-
-        newSpan.textContent = todoInput.value; // span 안에 value값 담기
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        // 여기 안에 “input 값 가져오기 → li 만들기 → ul에 추가”를 넣을 것
+        const text = input.value.trim();
+        if (!text) return;
+        const li = document.createElement("li"); // li를 만드는 코드
+        li.textContent = text; // 안전하고 추천
+        list.appendChild(li);
+        // 또는 list.append(li);
+        input.value = "";
+        input.focus();
     }
-}
-
-const wrap = document.querySelector('#wrap');
-console.log(wrap); //<section id="wrap">...</section
-
-const box2 = wrap.querySelector(".box2");
-console.log(box2); //<article class="box2"><a href="#">text02</a></article>
-
-const children = wrap.children;
-console.log(children); //[article.box1, article.box2, article.box3]
-
-// const children = wrap.children;
-// console.log(children[0]); //<article class="box1">text01</article>
-
-// const children = wrap.children;
-// console.log(children[1]); //<article class="box2"><a href="#">text02</a></article>
-
-// const children = wrap.children;
-// console.log(children[2]); //<article class="box3">text01</article>
+});
--- practice/day10.js
+++ practice/day10.js
@@ -17,3 +17,72 @@ console.log("a의 바로 부모:", a.parentElement);        // article.box2
 console.log("a에서 #wrap 찾기:", a.closest("#wrap"));  // section#wrap
 
 console.log("✅ 끝!");            
+
+const el = document.getElementById("box");
+console.log(el);
+el.textContent = "반가워"; // 안녕에서 반가워로 바뀜.
+
+document.body.style.backgroundColor = "lightyellow";
+
+document.title = "내 첫 웹페이지";
+
+[...document.images].forEach(img => {
+  img.style.border = "3px solid red";
+});
+
+console.log(document.forms.length); // 2
+console.log(document.forms[0].id);  // login
+
+[...document.links].forEach(link => {
+  link.target = "_blank";
+});
+
+console.log(document.scripts.length); // JS 파일의 개수
+
+document.head.style.backgroundColor = "lightgray";
+
+document.documentElement.style.fontSize = "20px";
+
+console.log(document.cookie);
+
+document.cookie = "username=subin";
+
+console.log("설정 후:", document.cookie);
+// 로컬 파일로 열어서 쿠키를 저장하지 못해 빈 상자처럼 나온다.
+
+console.log(document.URL);
+
+const $css = document.querySelector('.css');
+      
+      console.log($css.previousSibling);
+      console.log($css.nextSibling);
+      console.log($css.previousElementSibling);
+      console.log($css.nextElementSibling);
+
+const $languages = document.getElementById('languages');
+      
+      console.log($languages);
+      console.log($languages.childNodes);
+      console.log($languages.children);
+      console.log($languages.firstChild);
+      console.log($languages.lastChild);
+      console.log($languages.firstElementChild);
+      console.log($languages.lastElementChild);
+      console.log($languages.hasChildNodes());
+      console.log($languages.children.length);
+      console.log($languages.childElementCount);
+
+
+const input = document.querySelector('input[type=text]');
+     console.log(input);
+
+     if (!input.hasAttribute('value')) {  // value 어트리뷰트가 존재하지 않으면
+       // value 어트리뷰트를 추가하고 값으로 'hello!'를 설정
+       input.setAttribute('value', 'hello!');
+     }
+
+     // value 어트리뷰트 값을 취득
+     console.log(input.getAttribute('value')); // hello!
+
+     // value 어트리뷰트를 제거
+     input.removeAttribute('value');
